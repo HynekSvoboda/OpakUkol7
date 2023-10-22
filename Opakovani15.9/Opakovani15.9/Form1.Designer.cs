0eaaa11f21aa9b48f2586bc81aa20457534be1d8
HynekSvoboda <97514770+HynekSvoboda@users.noreply.github.com>
2023-10-22 12:55:32 +0200

    Add files via upload

---
+++ Opakovani15.9/Opakovani15.9/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Opakovani15._9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(48, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "n:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 122);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Opakovani15.9/Opakovani15.9/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Opakovani15._9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader cteni=null;
+            int n = int.Parse(textBox1.Text);
+
+            try
+            {
+                string[] radky = File.ReadAllLines("cisla.txt");
+                int cislo = int.Parse(radky[4]);
+                long cisloumocneni = 1;
+                double zaporneumocneni = 1;
+
+                try
+                {
+                    if (n > 0)
+                    {
+                        for (int i = 0; i < n; i++)
+                        {
+                            checked { cisloumocneni *= cislo; }
+                        }
+                        MessageBox.Show("mocnina : " + cisloumocneni);
+                    }
+                    else if (n < 0)
+                    {
+                        for (int i = 0; i > n; i--)
+                        {
+                            checked { zaporneumocneni /= (double)cislo; }
+                        }
+                        MessageBox.Show("mocnina : " + zaporneumocneni);
+                    }
+                    else MessageBox.Show("mocnina : " + cisloumocneni);
+                }
+                catch (ArithmeticException)
+                {
+                    MessageBox.Show("Preteceni pri umocnovani !!!");
+                }
+
+                double cislo2 = Convert.ToDouble(cislo);
+
+                try { MessageBox.Show("podil celeho cisla : " + checked(cislo / n)); }
+                catch (DivideByZeroException ex)
+                {
+                    MessageBox.Show("DivideByZeroException. " + ex.Message);
+                }
+                if (n != 0)
+                {
+                    cislo2 = cislo2 / n;
+                    MessageBox.Show("realny podil : " + cislo2);
+                }
+                else MessageBox.Show("n neodopovida !!!!");
+
+                int soucet = 0;
+                try
+                {
+                    cteni = new StreamReader("cisla.txt");
+                    while (!cteni.EndOfStream)
+                    {
+                        checked { soucet += Convert.ToInt32(cteni.ReadLine()); }
+                    }
+                    MessageBox.Show("Součet vsech cisel : " + soucet);
+                }
+                catch (ArithmeticException)
+                {
+                    MessageBox.Show("Preteceni pri souctu !!!");
+                }
+            }
+            catch (OverflowException ex)
+            {
+                MessageBox.Show("OverflowException. " + ex.Message);
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("FormatException. " + ex.Message);
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show("FileNotFoundException. " + ex.Message);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                MessageBox.Show("pate cislo neexistuje :-)");
+            }
+            finally
+            {
+                if (cteni != null) cteni.Close();
+            }
+        }
+    }
+}
